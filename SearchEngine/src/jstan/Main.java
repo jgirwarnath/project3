855a56544606d8d4de9b07ce439ed56d5a8e5b6a
thomas <thomas@Thomas>
2016-02-16 23:53:31 -0500

    Finishd product

--- SearchEngine/src/jstan/Main.java
+++ SearchEngine/src/jstan/Main.java
@@ -1,21 +1,52 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package jstan;
 
+import javax.swing.*;
+import java.awt.event.*;
+import java.awt.*;
+import javax.swing.SwingUtilities;
 /**
  *
- * @author Jonathan
+ * @author thomas
  */
-public class Main {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+class Main extends JFrame
+{
+	ImageIcon img = new ImageIcon("C:\\BoxGui\\face.jpg");
+	
+	public Main()
+	{
+	
+            JFrame frame = new JFrame();
+            frame.setIconImage(img.getImage());
+            frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+            SearchEngine se = new SearchEngine();
+            frame.add(se);
+            frame.setSize(700, 500);
+            //frame.pack();
+            frame.setLocationByPlatform(true);
+            frame.setVisible(true);
+            
+            JFrame frame2 = new JFrame();
+            frame2.setIconImage(img.getImage());
+            frame2.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+            Maintenance me = new Maintenance();
+            frame2.add(me);
+            frame2.setSize(600, 440);
+            //frame.pack();
+            frame2.setLocationByPlatform(true);
+            frame2.setVisible(true);
+            
+            
+        }
+        
+    public static void main(String[] args) 
+	{
+        SwingUtilities.invokeLater(new Runnable() 
+		{
+            public void run() 
+			{
+				Main mm = new Main();
+            }
+        });
     }
-    
 }
