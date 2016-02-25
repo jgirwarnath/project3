3f1a64c004c7c36ac7483bef43876f6a21f39437
Jonathan Girwar-Nath <>
2016-02-25 00:14:29 -0500

    Trying something new

--- SearchEngine/src/jstan/Main.java
+++ SearchEngine/src/jstan/Main.java
@@ -6,6 +6,9 @@
 package jstan;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
+import javax.swing.JFrame.*;
+
 /**
  *
  * @author Jonathan Girwar-Nath
@@ -17,13 +20,42 @@ public class Main {
      */
     public static void main(String[] args) {
         //setting up the components of the main screen
-       JFrame     frame  =  new JFrame("Java Search Engine");
-       JTextArea txtBox =  new JTextArea();    
-       JLabel     search   =  new JLabel("Enter your Search Terms: ");
-       frame.setSize(650, 500);
+       JFrame frame  =  new JFrame("Java Search Engine");
+       JTextArea resultsTxtBox =  new JTextArea();    
+       JTextArea searchLabel   =  new JTextArea("        Enter your Search Terms          ");
+       JLabel title = new JLabel("Search Engine");
+       JPanel titlePanel = new JPanel();
+       JPanel buttonPanel = new JPanel();
        
-       JLabel  TITLE   =  new JLabel("Search Engine");
+       //create the properties of the window: 
+       //size, location, visibility and exit action
+       frame.setSize(600, 500);
+       frame.setLocationRelativeTo(null);
+       frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+       frame.setVisible(true);
        
+       
+       //Create and layout the components
+       
+       frame.setLayout(new BorderLayout());
+       frame.add(titlePanel, "North");
+       searchLabel.setFont(new Font("Arial", Font.BOLD, 20));
+       titlePanel.add(searchLabel);
+       frame.add(new JScrollPane(resultsTxtBox),"Center");
+       frame.add(buttonPanel, "South");
+       JButton btn = new JButton ("Maintenence");
+       buttonPanel.add(btn);
+       
+       
+       
+       btn.addActionListener(new ActionListener() {
+           public void actionPerformed (ActionEvent ae){
+               System.out.println("comming soon");
+           }
+       });
+       
+       
+       /*
        //setting panels up
        JPanel main  = new JPanel(new BorderLayout()); 
        JPanel NORTH = new JPanel();
@@ -36,7 +68,7 @@ public class Main {
        //adding components to the north panel
        main.add(NORTH, BorderLayout.NORTH); 
        NORTH.setPreferredSize(new Dimension(200, 100));
-       NORTH.add(TITLE, BorderLayout.NORTH);
+       NORTH.add(title, BorderLayout.NORTH);
        NORTH.add(search, BorderLayout.SOUTH);
        txtBox.setPreferredSize(new Dimension(150, 15));
        NORTH.add(txtBox);
@@ -57,8 +89,7 @@ public class Main {
        JButton about = new JButton("About...");
        about.setLocation(640, 480);
        SOUTH.add(about, BorderLayout.WEST);
-       frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-       frame.setVisible(true);
+       */
     }
     
 }
