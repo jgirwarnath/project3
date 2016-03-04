125c8989377ef95849afa968014e7e9fea50aa01
Thomas Schlicher <tschlicher@yahoo.com>
2016-03-04 10:22:12 -0500

    updated searchengine

--- SearchEngine/src/jstan/Main.java
+++ SearchEngine/src/jstan/Main.java
@@ -15,30 +15,35 @@ import javax.swing.SwingUtilities;
 class Main extends JFrame
 {
 	//changing image located on JFrame
-	ImageIcon img = new ImageIcon("C:\\BoxGui\\face.jpg");
+	//ImageIcon img = new ImageIcon("C:\\BoxGui\\face.jpg");
 	
 	public Main()
 	{
             JFrame frame = new JFrame();
-            frame.setIconImage(img.getImage());
+            //frame.setIconImage(img.getImage());
             frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
             SearchEngine se = new SearchEngine();
             frame.add(se);
             frame.setSize(700, 500);
             frame.pack();
-            frame.setLocationByPlatform(true);
+            frame.setLocationRelativeTo(null);
             frame.setVisible(true);
             
             JFrame frame2 = new JFrame();
-            frame2.setIconImage(img.getImage());
+            //frame2.setIconImage(img.getImage());
             frame2.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
             Maintenance me = new Maintenance();
             frame2.add(me);
             frame2.setSize(600, 440);
             frame.pack();
-            frame2.setLocationByPlatform(true);
+            frame2.setLocationRelativeTo(null);
             frame2.setVisible(true);
             
+            
+        }
+        
+        public void frameTwo()
+        {
             
         }
         
