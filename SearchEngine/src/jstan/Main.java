b46b50f4314b743713331b08f9a5bcefb47d4084
Thomas Schlicher <'tschlicher@yahoo.com'>
2016-03-09 11:38:20 -0500

    updates on button actions

--- SearchEngine/src/jstan/Main.java
+++ SearchEngine/src/jstan/Main.java
@@ -14,40 +14,30 @@ import javax.swing.SwingUtilities;
  */
 class Main extends JFrame
 {
-	//changing image located on JFrame
-	//ImageIcon img = new ImageIcon("C:\\BoxGui\\face.jpg");
-        static JFrame frame;
-        static JFrame frame2;
+        static JFrame searchFrame;
+        static JFrame maintenanceFrame;
 	
 	public Main()
 	{
-            frame = new JFrame();
-            //frame.setIconImage(img.getImage());
-            frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+            searchFrame = new JFrame();
+            searchFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
             SearchEngine se = new SearchEngine();
-            frame.add(se);
-            frame.setSize(700, 500);
-            frame.pack();
-            frame.setLocationRelativeTo(null);
-            frame.setVisible(true);
+            searchFrame.add(se);
+            searchFrame.setSize(700, 500);
+            searchFrame.pack();
+            searchFrame.setLocationRelativeTo(null);
+            searchFrame.setVisible(true);
             
-             frame2 = new JFrame();
-            //frame2.setIconImage(img.getImage());
-            frame2.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+             maintenanceFrame = new JFrame();
+            maintenanceFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
             Maintenance me = new Maintenance();
-            frame2.add(me);
-            frame2.setSize(600, 440);
-            frame.pack();
-            frame2.setLocationRelativeTo(null);
-            frame2.setVisible(false);
+            maintenanceFrame.add(me);
+            maintenanceFrame.setSize(600, 440);
+            searchFrame.pack();
+            maintenanceFrame.setLocationRelativeTo(null);
+            maintenanceFrame.setVisible(false);
             
             
-        }
-        
-        public void reset()
-        {
-            frame.setLocationRelativeTo(null);
-            frame2.setLocationRelativeTo(null);
         }
         
     public static void main(String[] args) 
