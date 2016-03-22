291ffeb0f8a7db98ec47cf529d23c52af90fcec2
scerome2 <scerome2@DTEC427-R1-06.academic.hccfl.edu>
2016-03-22 19:54:46 -0400

    Saurel's commit testing project 5....inverted index

--- SearchEngine/src/jstan/Main.java
+++ SearchEngine/src/jstan/Main.java
@@ -7,15 +7,65 @@ package jstan;
 
 /**
  *
- * @author Jonathan
+ * @author Saurel
  */
-public class Main {
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.*;
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
-    
-}
+
+/**
+	 * Launch the application.
+	 */
+public class Main {
+	
+	public static void main(String [] args){
+		
+		SearchEngine bas = new SearchEngine();
+		bas.frame.setVisible(true);
+		
+		
+		Maintenance maint = new Maintenance();
+		maint.frame.setVisible(true);
+		
+		
+		FileInputStream  fileIn = null;
+		FileOutputStream fileOut = null;
+		
+		try{
+			fileIn = new FileInputStream("texts/input");
+			fileOut = new FileOutputStream("texts/output");
+			
+			int i;
+			while((i = fileIn.read())!=-1){
+				
+				fileOut.write(i);
+			}
+			
+			
+		} catch (IOException ioe){
+			
+		}
+		
+		finally {
+				try{
+					if(fileIn !=null) {fileIn.close();}
+					if(fileOut !=null) {fileOut.close();}
+					
+						
+				}
+				catch(IOException ioe){
+					
+				}
+		}
+		
+	}
+
+		
+		
+		
+		
+	}
+	
+
