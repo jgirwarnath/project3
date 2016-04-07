c8f48a447e23c5d43c7de7e5a81c16b8e041fefc
Thomas Schlicher <'tschlicher@yahoo.com'>
2016-04-07 09:31:03 -0500

    Added new mock class for the implementation of the Boolean searching. Feel free to adjust anything as this is only mock classes with few comments.

---
+++ SearchEngine/jstan/BooleanSeacrh.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jstan;
+
+/**
+ *
+ * @author thomas
+ */
+public class BooleanSeacrh {
+    
+    //feel free to change anything, I'm just tyring to get the basics written out
+    
+    //this class should handle the searching aspect of the program.
+    
+    //"and", "or", and "exact phrase"
+}
