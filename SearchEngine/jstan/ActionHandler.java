512051fc50581957fa71d011da97a87aeed03f48
Thomas Schlicher <'tschlicher@yahoo.com'>
2016-04-11 08:45:09 -0500

    Working on loading JTable and saving ID for each file indexed

--- SearchEngine/jstan/ActionHandler.java
+++ SearchEngine/jstan/ActionHandler.java
@@ -4,14 +4,22 @@
  
 package jstan;
 
+import java.util.List;
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.stream.Collectors;
 import javax.swing.JFileChooser;
 import javax.swing.JOptionPane;
 import javax.swing.table.DefaultTableModel;
@@ -22,6 +30,7 @@ import javax.swing.table.DefaultTableModel;
  */
 public class ActionHandler
 {
+    private Object[][] fileList;
     static int ID = 0;
     String index = "Indexed";
     DefaultTableModel dtm;
@@ -45,24 +54,25 @@ public class ActionHandler
         if (result == JFileChooser.APPROVE_OPTION)
         {
             // here we will add files to JTable and then call a method to save them.
-            File selectedFile = fileChooser.getSelectedFile(); 
+            File selectedFile = fileChooser.getSelectedFile();
             dtm = (DefaultTableModel) Maintenance.table.getModel();
-            System.out.println("Selected file: " + selectedFile.getAbsolutePath());
-            dtm.addRow(new Object[]{selectedFile.getAbsoluteFile(), index});
+            System.out.println("fileList " + selectedFile.getAbsolutePath());
+            dtm.addRow(new Object[]{selectedFile.getAbsolutePath(), index});
             
             saveJTable();
         }
     }
     
     public void removeFileRow()
-    { 
+    {
         DefaultTableModel remove = (DefaultTableModel) Maintenance.table.getModel();
-        if(Maintenance.table.getRowCount()  == 0)
+        if(Maintenance.table.getRowCount() == 0)
         {
            JOptionPane.showMessageDialog(null, "No files to remove", "Warning", JOptionPane.WARNING_MESSAGE);  
         }
         else{      
-        remove.removeRow(Maintenance.table.getSelectedRow());
+            
+            remove.removeRow(Maintenance.table.getSelectedRow());
         }
     }
     
@@ -70,21 +80,18 @@ public class ActionHandler
     // be loaded into JTable.
     public void saveJTable()
     {
+ 
         try {
 
             toFile = new FileWriter("D:\\JavaSearchEngine\\part3\\SearchEngine\\resources\\JTableSaveInfo.txt", true);
-
+            
             for(int row = 0; row < Maintenance.table.getRowCount(); row++)
-            {
-                ID++;
-                toFile.write(String.valueOf(ID));
-                toFile.write("\t");
-                    
+            {                   
                 for(int col = 0; col < Maintenance.table.getColumnCount(); col++)
                 {
                     toFile.write(Maintenance.table.getValueAt(row, col).toString()+"\t");
                 }
-               toFile.write("\n");
+                toFile.write("\n");
             }
             toFile.close();
             }catch (IOException ex) 
@@ -95,6 +102,33 @@ public class ActionHandler
     
    public void loadJTable()
     {
+        DefaultTableModel loadTable = (DefaultTableModel) Maintenance.table.getModel();
+        String line = null;
+        //trying out how this works
+        try (BufferedReader br = Files.newBufferedReader(Paths.get("D:\\JavaSearchEngine\\part3\\SearchEngine\\resources\\JTableSaveInfo.txt"))) {
+            
+            br.readLine();
+            br.readLine();
+            while( (line = br.readLine() ) != null ) {
+                        loadTable.addRow( line.split("\t") );
+            }
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
        
     }
 }
+
+/*List<String> newList = new ArrayList<>();
+
+		try (BufferedReader br = Files.newBufferedReader(Paths.get("D:\\JavaSearchEngine\\part3\\SearchEngine\\resources\\JTableSaveInfo.txt"))) {
+
+		//br returns as stream and convert it into a List
+		newList = br.lines().collect(Collectors.toList());
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	
+		newList.forEach(System.out::println);*/
