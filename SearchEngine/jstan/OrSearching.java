7e1a1a4c39e6131943ecdec55971711ea8a26319
Thomas Schlicher <'tschlicher@yahoo.com'>
2016-04-21 13:04:29 -0500

    working on retrieving text by user for searching  and displaying it in the maintenance table.

---
+++ SearchEngine/jstan/OrSearching.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jstan;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author thomas
+ */
+public class OrSearching 
+{
+    String wordsToSearch = SearchEngine.schTxt.getText();
+    
+    public void outPut()
+    {
+          System.out.println(wordsToSearch);
+    }
+}
